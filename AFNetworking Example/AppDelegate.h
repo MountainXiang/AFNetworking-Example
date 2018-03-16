f1ac6b58e747e85885750bc9319dde9d9661fa55
dashan.xiang <783600983@qq.com>
2018-03-16 17:31:19 +0800

    Initial Project

---
+++ AFNetworking Example/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AFNetworking Example
+//
+//  Created by MountainX on 2018/3/16.
+//  Copyright © 2018年 MTX Software Technology Co.,Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AFNetworking Example/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AFNetworking Example
+//
+//  Created by MountainX on 2018/3/16.
+//  Copyright © 2018年 MTX Software Technology Co.,Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
